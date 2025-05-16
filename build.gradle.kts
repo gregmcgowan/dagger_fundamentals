d66bc8f98f6ed078a5bb4958967b40d8b57d7ea0
Greg McGowan <gregmcgowan@gmail.com>
2025-05-16 15:17:58 +0100

    Use kapt instead of ksp so we can use scabbard to generate diagrams

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,23 @@ import org.gradle.kotlin.dsl.implementation
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.kapt)
     alias(libs.plugins.ktLint)
+    alias(libs.plugins.scabbard)
+}
+
+// required for scabbard https://github.com/arunkumar9t2/scabbard/issues/92#issuecomment-901107022
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "com.github.kittinunf.result" && requested.name == "result" && requested.version == "3.0.0") {
+            useVersion("3.0.1")
+            because("Transitive dependency of Scabbard, currently not available on mavenCentral()")
+        }
+    }
+}
+
+scabbard {
+    enabled = true
 }
 
 repositories {
@@ -17,5 +32,5 @@ kotlin {
 dependencies {
     implementation(libs.dagger)
     implementation(libs.okHttp)
-    ksp(libs.dagger.compiler)
+    kapt(libs.dagger.compiler)
 }
